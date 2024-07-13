42b9085aa569cb392b48bef91953f62a3122abf0
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-13 16:24:59 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Course 1");
+        System.out.println("Theme 1");
+        System.out.println("Task 1");
+        int numOne = 47;
+        int numTwo = 13;
+        int division = numOne/numTwo;
+        int remainder = numOne%numTwo;
+        System.out.println("Результат деления числа " + numOne + " на " + numTwo + ": " + division + " и " + remainder + " в остатке");
     }
 }
