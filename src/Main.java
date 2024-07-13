90c05bd9a0a7d83fd9f14aea256bda9b4abea110
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-13 16:32:35 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         int division = numOne/numTwo;
         int remainder = numOne%numTwo;
         System.out.println("Результат деления числа " + numOne + " на " + numTwo + ": " + division + " и " + remainder + " в остатке");
+        System.out.println("Task 2");
+        numOne = 36;
+        int figureOne = numOne/10;
+        int figureTwo = numOne - figureOne*10;
+        int sum = figureOne + figureTwo;
+        System.out.println("Результат сложения цифр первого и второго разрядов числа " + numOne + ": " + sum);
     }
 }
